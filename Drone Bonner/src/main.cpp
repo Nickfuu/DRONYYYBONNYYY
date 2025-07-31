883a22bcb89e3e719fdd799174c0d035a3790f23
Nikola Rebic <160264942+Nickfuu@users.noreply.github.com>
2025-07-30 22:36:45 -0700

    ServoLibraryAdd

--- Drone Bonner/src/main.cpp
+++ Drone Bonner/src/main.cpp
@@ -1,7 +1,5 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
 
 void setup() {
   // put your setup code here, to run once:
@@ -12,7 +10,3 @@ void loop() {
   // put your main code here, to run repeatedly:
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
